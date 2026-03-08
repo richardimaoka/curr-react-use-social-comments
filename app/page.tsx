0e751c5ac08548b28a2c3154821297b36f82bd4b
richardimaoka <richard.s.imaoka@gmail.com>
2026-03-08 10:57:12 +0900

    cat pic

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
 import CommentPopup from "./components/CommentPopup";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>My Video Page</h1>
-      <CommentPopup videoId="video-123" />
+      <Image
+        src="/images/cat.webp"
+        alt="Cat"
+        className={styles.imageContainer}
+        width={1080}
+        height={1920} // This will be overridden by CSS
+      />
+      <div className={styles.contents}>
+        <CommentPopup videoId="video-123" />
+      </div>
     </div>
   );
 }
