de26413f0f2265c7ce1e7d325b55a5b5008183b7
richardimaoka <richard.s.imaoka@gmail.com>
2026-03-08 10:30:51 +0900

    centering

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import CommentPopup from './components/CommentPopup';
+import CommentPopup from "./components/CommentPopup";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.page}>
       <h1>My Video Page</h1>
       <CommentPopup videoId="video-123" />
     </div>
   );
 }
-
